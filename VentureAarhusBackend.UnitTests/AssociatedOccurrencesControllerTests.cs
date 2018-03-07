d750d874bd5217c71394baeff730ffd391742943
benjaminoerskov <ben.oe@icloud.com>
2018-03-07 09:49:19 +0100

    Test cleanup + another test

--- VentureAarhusBackend.UnitTests/AssociatedOccurrencesControllerTests.cs
+++ VentureAarhusBackend.UnitTests/AssociatedOccurrencesControllerTests.cs
@@ -22,6 +22,14 @@ namespace VentureAarhusBackend.UnitTests
     public class AssociatedOccurrencesControllerTests
     {
         private AssociatedOccurrencesController _controller = null;
+        private Mock<IMapper> _mockMapper = null;
+        private ApplicationUser _testUser = null;
+
+        private DbContextOptionsBuilder<ApplicationDbContext> _optionsBuilder = null;
+        private ApplicationDbContext _dbContext = null;
+
+        private Mock<Repository<AssociatedOccurrences>> _mockRepo = null;
+
 
         [TestInitialize]
         public void SetUp()
@@ -29,22 +37,19 @@ namespace VentureAarhusBackend.UnitTests
             var mockUserStore = new Mock<IUserStore<ApplicationUser>>();
             var mockUserManager = new Mock<UserManager<ApplicationUser>>(mockUserStore.Object, null, null, null, null, null, null, null, null);
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseInMemoryDatabase();
-            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+            _optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase();
+            //_optionsBuilder.UseInMemoryDatabase();
+            _dbContext = new ApplicationDbContext(_optionsBuilder.Options);
 
-            var mockRepo = new Mock<Repository<AssociatedOccurrences>>(dbContext);
-            var mockMapper = new Mock<IMapper>();
+            _mockRepo = new Mock<Repository<AssociatedOccurrences>>(_dbContext);
 
-            //Setup methods:
-
-            var testUser = new ApplicationUser()
+            _mockMapper = new Mock<IMapper>();
+            _controller = new AssociatedOccurrencesController(mockUserManager.Object, _mockRepo.Object, _mockMapper.Object);
+            _testUser = new ApplicationUser()
             {
                 Email = "test@test.com",
                 Id = "testID"
             };
-            
-            _controller = new AssociatedOccurrencesController(mockUserManager.Object, mockRepo.Object, mockMapper.Object);
 
             _controller.ControllerContext = new ControllerContext
             {
@@ -52,44 +57,48 @@ namespace VentureAarhusBackend.UnitTests
                 {
                     User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
                     {
-                        new Claim(ClaimTypes.Email, testUser.Email),
+                        new Claim(ClaimTypes.Email, _testUser.Email),
                     }))
                 }
             };
 
-            var occurrencesList = new List<AssociatedOccurrences>();
-
-            var associated1 = new AssociatedOccurrences
-            {
-                ApplicationUser = testUser,
-                Id = 30,
-                ApplicationUserId = testUser.Id,
-                Type = "like"
-            };
-            occurrencesList.Add(associated1);
-            mockRepo.Setup(x => x.GetAll()).Returns(occurrencesList.AsQueryable());
-            mockUserManager.Setup(x => x.GetUserAsync(_controller.User)).Returns(Task.FromResult<ApplicationUser>(testUser));
-
-            var AssociatedDTO = new AssociatedOccurrencesDTO
-            {
-                ApplicationUserId = testUser.Id,
-                Id = 500,
-                OccurrenceId = 20,
-                Type = "like"
-            };
-
-            mockMapper.Setup(x =>
-                x.Map<AssociatedOccurrencesDTO>(It.IsAny<AssociatedOccurrences>())).Returns(AssociatedDTO);
+            //Setup methods:
+            mockUserManager.Setup(x => x.GetUserAsync(_controller.User)).Returns(Task.FromResult<ApplicationUser>(_testUser)); 
         }
 
         [TestMethod]
-        public void PostAssociatedOccurrence()
+        public void PostAssociatedOccurrencePositive()
         {
             var param = new PostAssociatedOccurrencesParams
             {
                 OccurrenceId = 20,
                 TypeOfAssociation = "like"
             };
+
+            var AssociatedDTO = new AssociatedOccurrencesDTO
+            {
+                ApplicationUserId = _testUser.Id,
+                Id = 500,
+                OccurrenceId = 20,
+                Type = "like"
+            };
+
+            var occurrencesList = new List<AssociatedOccurrences>();
+
+            var associated1 = new AssociatedOccurrences
+            {
+                ApplicationUser = _testUser,
+                Id = 30,
+                ApplicationUserId = _testUser.Id,
+                Type = "like",
+                OccurrenceId = 1
+            };
+            occurrencesList.Add(associated1);
+            _mockRepo.Setup(x => x.GetAll()).Returns(occurrencesList.AsQueryable());
+
+            _mockMapper.Setup(x =>
+                x.Map<AssociatedOccurrencesDTO>(It.IsAny<AssociatedOccurrences>())).Returns(AssociatedDTO);
+
 
             var response = _controller.PostAssociatedOccurrence(param).Result;
             var okResult = response.Should().BeOfType<OkObjectResult>().Subject;
@@ -98,6 +107,45 @@ namespace VentureAarhusBackend.UnitTests
             occurrences.Type.Should().Be("like");
             occurrences.Id.Should().Be(500);
             occurrences.OccurrenceId.Should().Be(20);
+        }
+
+        [TestMethod]
+        public void PostAssociatedOccurrenceNegative()
+        {
+            var param = new PostAssociatedOccurrencesParams
+            {
+                OccurrenceId = 20,
+                TypeOfAssociation = "like"
+            };
+
+            var AssociatedDTO = new AssociatedOccurrencesDTO
+            {
+                ApplicationUserId = _testUser.Id,
+                Id = 500,
+                OccurrenceId = 20,
+                Type = "like"
+            };
+
+            var occurrencesList = new List<AssociatedOccurrences>();
+
+            var associated1 = new AssociatedOccurrences
+            {
+                ApplicationUser = _testUser,
+                Id = 30,
+                ApplicationUserId = _testUser.Id,
+                Type = "like",
+                OccurrenceId = 20
+            };
+            occurrencesList.Add(associated1);
+
+            _mockRepo.Setup(x => x.GetAll()).Returns(occurrencesList.AsQueryable());
+
+            _mockMapper.Setup(x =>
+                x.Map<AssociatedOccurrencesDTO>(It.IsAny<AssociatedOccurrences>())).Returns(AssociatedDTO);
+
+
+            var response = _controller.PostAssociatedOccurrence(param).Result;
+            var okResult = response.Should().BeOfType<BadRequestObjectResult>().Subject;
         }
 
         [TestCleanup]
